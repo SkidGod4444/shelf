f4fa0548424ece2f77ac1316560d1f0c8175526f
Saidev Dhal <connect.saidev@gmail.com>
2025-02-26 11:04:44 +0530

    add content

--- components/custom/shelfs/videshelf.tsx
+++ components/custom/shelfs/videshelf.tsx
@@ -121,7 +121,7 @@ export default function VideoshelfContent() {
       {Object.entries(videosByCategory).map(([category, videos]) => (
         <section key={category}>
           <h2 className="text-3xl font-serif my-8">{category}</h2>
-          <div className="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
             {videos.map((video) => (
               <VideoCard key={video.id} video={video} />
             ))}
--- config/books.ts
+++ config/books.ts
@@ -9,6 +9,12 @@ export interface Book {
 
   export const books: Book[] = [
     {
+      id: 3,
+      category: "Backend System Design",
+      cover: '/images/books/dda.png',
+      downloadLink: "https://drive.google.com/file/d/1zbqhUvFrUcnMNO34W6s8lc1zx0G-NXcT/view?usp=sharing",
+    },
+    {
       id: 1,
       category: "Personal Productivity and Growth",
       cover: '/images/books/ath.png',
@@ -21,15 +27,15 @@ export interface Book {
       buyLink: "https://amzn.in/d/ivVZgSW",
     },
     {
-      id: 3,
-      category: "Backend System Design",
-      cover: '/images/books/dda.png',
-      downloadLink: "https://drive.google.com/file/d/1zbqhUvFrUcnMNO34W6s8lc1zx0G-NXcT/view?usp=sharing",
-    },
-    {
       id: 4,
       category: "Spirituality and Consciousness",
       cover: '/images/books/sexm.png',
        downloadLink: "https://drive.google.com/file/d/1vqYsHaMM2fKCZNJtRLupiDI9Jq41TXd8/view?usp=sharing",
+    },
+    {
+      id: 5,
+      category: "Spirituality and Consciousness",
+      cover: '/images/books/mti.png',
+       downloadLink: "https://drive.google.com/file/d/1vm59hwMy78V3sT-viJW9EX_dAWJJbR40/view?usp=sharing",
     },
   ]
