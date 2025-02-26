9024a2e36d00d73c28ab4ad5fdaa4b317f270316
Saidev Dhal <connect.saidev@gmail.com>
2025-02-26 11:44:04 +0530

    add content

--- config/papers.ts
+++ config/papers.ts
@@ -7,11 +7,29 @@ export interface Paper {
   category: string
   downloadLink: string
   publishedIn?: string
-  year: number
+  year?: number
 }
 
 
 export const papers: Paper[] = [
+  {
+    id: 2,
+    title: "Introduction to React in 30 bite-size Morsels",
+    category: "React Internals",
+    cover: "/images/papers/30react.png",
+    downloadLink: "https://drive.google.com/file/d/12vxQUnyxWn4AHCp9kkayeDSsHCqJ-qGR/view?usp=sharing",
+    publishedIn: "Fullstack.io",
+    // year: 2009,
+  },
+  {
+    id: 4,
+    title: "Didact: Build a Miniature React with Hooks",
+    category: "React Internals",
+    cover: "/images/papers/byr.png",
+    downloadLink: "https://pomb.us/build-your-own-react/",
+    publishedIn: "Rodrigo Pombo",
+    year: 2019,
+  },
   {
     id: 1,
     title: "Bitcoin: A Peer-to-Peer Electronic Cash System",
